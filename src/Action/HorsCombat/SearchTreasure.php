6e6f427d26595420277077d42d9ded480e5630ed
alvin.flattot <alvin.flattot@interstis.fr>
2025-06-05 15:25:36 +0200

    ajout de la liste des items treasure

--- src/Action/HorsCombat/SearchTreasure.php
+++ src/Action/HorsCombat/SearchTreasure.php
@@ -14,19 +14,33 @@ class SearchTreasure implements ActionInterface
     public function execute(Character $actor, ?Character $target, array $params): array
     {
         $location      = $params['location'];
-        $listOfAwards  = $params['listOfAwards'];
-        $randomIndex   = array_rand($listOfAwards);
-        $award         = $listOfAwards[$randomIndex];
+        $awardDifficulty = $params['awardDifficulty'];
+        $listOfAwards  = $params['listOfAwards'] ?? [];
+        $randomTreasureValue  = rand(1, 100);
+        $treasureRoll = $params['treasureRoll'];
 
-        $resultDice = 10;
+        if (!isset($listOfAwards)) {
+            $listOfAwards[] = match (true) {
+                $randomTreasureValue <= 30 => ['name' => 'Une bourse de pièces d\'or'],
+                $randomTreasureValue <= 60 => ['name' => "4d6 pièces d’argent (pa)"],
+                $randomTreasureValue <= 70 => ['name' => "3d6 pièces d’or (po)"],
+                $randomTreasureValue <= 75 => ['name' => "2d6 po + une gemme commune (valeur 10 po)"],
+                $randomTreasureValue <= 80 => ['name' => "3d6 po + un objet d'art (valeur 25 po)"],
+                $randomTreasureValue <= 85 => ['name' => "2d6 po + un parchemin de sort de niveau 1 (ex : Détection de la magie)"],
+                $randomTreasureValue <= 90 => ['name' => "3d6 po + une potion de soin"],
+                $randomTreasureValue <= 95 => ['name' => "1d6 po + 1 objet magique mineur (Table A*)"],
+                $randomTreasureValue <= 99 => ['name' => "1d6 po + 1 objet magique inhabituel (Table B*)"],
+                $randomTreasureValue <= 100 => ['name' => "Trésor spécial : 1d6 po + objet unique, mystérieux ou lié à une quête"],
+                default => [],
+            };
+        }
 
-        if ($resultDice >= $award['minimumDice']) {
-            $msg = "Le joueur fouille dans « $location », il obtient : « {$award['name']} »";
+        if ($treasureRoll >= $awardDifficulty) {
+            $msg = "Le joueur fouille dans « $location », il obtient : « {$listOfAwards} »"; //TODO: boucler pour afficher chaque récompense
         }
         else {
             $msg = "Le joueur fouille dans « $location », mais ne trouve rien.";
         }
-
 
         return [
             'events' => [$msg],
